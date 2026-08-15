2b6071b60ea78a4ef3bd9950389625c086092648
Kracken <Ejterhune84@gmail.com>
2026-08-15 23:35:45 +0000

    Use Wix member name for Labyrinth player

--- web-client/src/components/PlayerNamePanel.vue
+++ web-client/src/components/PlayerNamePanel.vue
@@ -1,5 +1,9 @@
 <template>
-    <v-editable-field v-if="isEditable" v-model="playerName" :placeholder="editablePlaceholder" />
+    <v-editable-field
+        v-if="isEditable"
+        v-model="playerName"
+        :placeholder="editablePlaceholder"
+    />
     <p v-else>{{ playerLabel }}</p>
 </template>
 
@@ -12,37 +16,79 @@ import { usePlayersStore } from "@/stores/players.js";
 
 export default {
     name: "player-name-panel",
+
     components: {
         VEditableField,
     },
+
     data() {
         return {
-            editablePlaceholder: "You (edit here)",
+            editablePlaceholder: "Player",
         };
     },
+
     props: {
         player: {
             required: true,
         },
     },
+
     computed: {
-        isEditable: function () {
+        isEditable() {
             return this.player.isUser;
         },
-        playerLabel: function () {
+
+        playerLabel() {
             return getLabel(this.player);
         },
+
         playerName: {
             get() {
                 return this.player.name;
             },
+
             set(value) {
                 this.changeUserPlayerName(value);
             },
         },
     },
+
     methods: {
         ...mapActions(usePlayersStore, ["changeUserPlayerName"]),
+
+        receiveWixPlayerName(event) {
+            const data = event.data;
+
+            if (!data || data.type !== "SET_PLAYER_NAME") {
+                return;
+            }
+
+            if (!this.player.isUser) {
+                return;
+            }
+
+            if (!data.playerName) {
+                return;
+            }
+
+            const cleanName = String(data.playerName).trim();
+
+            if (!cleanName) {
+                return;
+            }
+
+            console.log("Wix player name received:", cleanName);
+
+            this.changeUserPlayerName(cleanName);
+        },
+    },
+
+    mounted() {
+        window.addEventListener("message", this.receiveWixPlayerName);
+    },
+
+    beforeUnmount() {
+        window.removeEventListener("message", this.receiveWixPlayerName);
     },
 };
 </script>
